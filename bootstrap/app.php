69415d2f58fb0d1212922cc1413b76cc3a399690
Reza Satria Pangestu <rezasatriapangestu@gmail.com>
2026-02-09 15:20:14 +0700

    fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
             'check.menu' => \App\Http\Middleware\CheckMenuAccess::class,
