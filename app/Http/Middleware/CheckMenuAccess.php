9f7803896b327bc29bb4f7cbdacc1a021821e50f
Reza Satria Pangestu <rezasatriapangestu@gmail.com>
2026-02-18 11:24:19 +0700

    debug 403

--- app/Http/Middleware/CheckMenuAccess.php
+++ app/Http/Middleware/CheckMenuAccess.php
@@ -20,10 +20,13 @@ class CheckMenuAccess
         // Log access attempt
         \Illuminate\Support\Facades\Log::info("CheckMenuAccess: User " . ($request->user()->id ?? 'Guest') . " accessing menu ID: $menuId. Allowed: " . json_encode($allowed_menus));
 
-        if ($allowed_menus && in_array((int)$menuId, $allowed_menus)) {
+        // Broaden check to allow string or int matches
+        if ($allowed_menus && (in_array($menuId, $allowed_menus) || in_array((string)$menuId, $allowed_menus) || in_array((int)$menuId, $allowed_menus))) {
+             \Illuminate\Support\Facades\Log::info("CheckMenuAccess: ACCESS GRANTED for Menu ID $menuId");
              return $next($request);
         }
         
+        \Illuminate\Support\Facades\Log::warning("CheckMenuAccess: ACCESS DENIED for Menu ID $menuId");
         $debugInfo = "Menu ID: $menuId . Allowed: " . json_encode($allowed_menus);
         abort(403, 'ANDA TIDAK MEMILIKI HAK AKSES. ' . $debugInfo);
     }
